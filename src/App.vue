ed086c5848400ab681fe27808a87122f81a5823e
Arie-Shaver <143853683+Arie-Shaver@users.noreply.github.com>
2023-12-20 22:59:01 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// @group Template
+// Root of the Vue app; the main template of our project
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+
+@font-face {
+  font-family: "Levitation";
+  src: url('/assets/Levitation.ttf');
+}
+#app {
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+
+</style>
